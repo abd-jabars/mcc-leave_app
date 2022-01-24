c9d03d2edfe591431ef3b661b5442941fe120081
Syed Abdul Jabar <7abdjabar@gmail.com>
2022-01-25 06:18:46 +0700

    fix typo in leave employee model

--- API/Models/LeaveEmployee.cs
+++ API/Models/LeaveEmployee.cs
@@ -13,7 +13,7 @@ namespace API.Models
         public int Id { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public string Description { get; set; }
+        public string Attachment { get; set; }
 
         [JsonIgnore]
         public virtual Employee Employee { get; set; }
