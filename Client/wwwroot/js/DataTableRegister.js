8d479ceb3e92453548095c494faab6771f471390
Syed Abdul Jabar <7abdjabar@gmail.com>
2022-01-26 13:54:04 +0700

    register new employee
    using:
    - font awesome
    - sweet alert
    - data table

--- Client/wwwroot/js/DataTableRegister.js
+++ Client/wwwroot/js/DataTableRegister.js
@@ -1,5 +1,5 @@
 ﻿$(document).ready(function () {
-    $('#tes').DataTable({
+    $('#dataTableRegister').DataTable({
         'ajax': {
             'url': "https://localhost:44316/API/Employees/RegisteredData",
             'dataSrc': 'result'
@@ -56,4 +56,122 @@
             }
         ]
     });
+});
+
+function GetDepartmentManager() {
+    $.ajax({
+        url: "https://localhost:44316/API/Departments"
+
+    }).done((result) => {
+        var departmentOptions = "";
+
+        $.each(result.result, function (key, val) {
+            departmentOptions += `<option value="${val.id}">${val.name}</option>`
+        });
+        $("#department").html(departmentOptions);
+
+        var managerOptions = "";
+
+        $.each(result.result, function (key, val) {
+            managerOptions += `<option value="${val.managerId}">${val.managerId}</option>`
+        });
+        $("#manager").html(managerOptions);
+
+    }).fail((error) => {
+        console.log(error);
+    });
+}
+
+function Insert() {
+    GetDepartmentManager();
+    console.log("tes");
+
+    $('#password').attr("readonly", false);
+
+    $('#employeeForm').trigger("reset");
+        
+    var insertTitle = "";
+    insertTitle += `<h3 class="mx-auto my-1"> Register an Employee </h3>`;
+    $("#employeeModal .modal-header").html(insertTitle);
+
+    $("#submitButton").html("Insert");
+
+}
+
+function RegisterEmployee() {
+    var firstName = $('#firstName').val();
+    var lastName = $('#lastName').val();
+    var phone = $('#phone').val();
+    var email = $('#email').val();
+    var password = $('#password').val();
+    var birthDate = $('#birthDate').val();
+    var gender = $('#gender').val();
+    var department = $('#department').val();
+    var manager = $('#manager').val();
+
+    var register = Object();
+    register.FirstName = firstName;
+    register.LastName = lastName;
+    register.Phone = phone;
+    register.Email = email;
+    register.Password = password;
+    register.BirthDate = birthDate;
+    register.Gender = gender;
+    register.Department = department;
+    register.Manager = manager;
+
+    console.log(register);
+
+    var myTable = $('#dataTableRegister').DataTable();
+    $.ajax({
+        url: "https://localhost:44316/API/Employees/Register",
+        contentType: "application/json;charset=utf-8",
+        type: "POST",
+        data: JSON.stringify(register)
+        //data: register
+    }).done((result) => {
+        console.log(result);
+        console.log(result.result);
+        console.log(result.status);
+        console.log(result.message);
+
+        myTable.ajax.reload();
+        var swalIcon;
+        if (result.status == 200) {
+            swalIcon = 'success';
+            swalTitle = 'Success';
+        } else {
+            swalIcon = 'error'
+            swalTitle = 'Oops!'
+        }
+        Swal.fire({
+            icon: swalIcon,
+            title: swalTitle,
+            text: result.message,
+        });
+    }).fail((error) => {
+        console.log(error);
+        Swal.fire({
+            icon: 'error',
+            title: 'Something went wrong',
+            text: "Hmmm....",
+        });
+    });
+
+}
+
+$('#employeeForm').submit(function (e) {
+    e.preventDefault();
+    if ($("#submitButton").html() == "Insert") {
+        RegisterEmployee();
+        $('#employeeForm').trigger("reset");
+        $('#employeeModal').modal('hide');
+    } else {
+        UpdateData();
+        $('#employeeForm').trigger("reset");
+        $('#employeeModal').modal('hide');
+    }
+    //RegisterEmployee();
+    //$('#employeeForm').trigger("reset");
+    //$('#employeeModal').modal('hide');
 });
