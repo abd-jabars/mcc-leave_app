80498954c17aeb189dabdf5e620a0ed854e5be18
Leon <raviousha@gmail.com>
2022-01-28 03:38:10 +0700

    update datepicker

--- Client/wwwroot/js/leaveRequest.js
+++ Client/wwwroot/js/leaveRequest.js
@@ -278,3 +278,37 @@ $('#insertModal').on('hidden.bs.modal', function (e) {
         .find("input[type=text]")
         .val("");
 })
+
+$(function () {
+    var dateFormat = "mm/dd/yy",
+        from = $("#startDate")
+            .datepicker({
+                defaultDate: "+1w",
+                changeMonth: true,
+                numberOfMonths: 1,
+                beforeShowDay: $.datepicker.noWeekends
+            })
+            .on("change", function () {
+                to.datepicker("option", "minDate", getDate(this));
+            }),
+        to = $("#endDate").datepicker({
+            defaultDate: "+1w",
+            changeMonth: true,
+            numberOfMonths: 1,
+            beforeShowDay: $.datepicker.noWeekends
+        })
+            .on("change", function () {
+                from.datepicker("option", "maxDate", getDate(this));
+            });
+
+    function getDate(element) {
+        var date;
+        try {
+            date = $.datepicker.parseDate(dateFormat, element.value);
+        } catch (error) {
+            date = null;
+        }
+
+        return date;
+    }
+});
