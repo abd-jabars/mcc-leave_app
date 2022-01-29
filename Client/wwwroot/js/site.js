716c4beeecae27db98c6259c9a9d587662ceec85
Syed Abdul Jabar <7abdjabar@gmail.com>
2022-01-29 20:26:12 +0700

    add email notification using js

--- Client/wwwroot/js/site.js
+++ Client/wwwroot/js/site.js
@@ -16,3 +16,38 @@
 }).fail((error) => {
     console.log(error)
 })
+
+$.ajax({
+    url: "/LeaveEmployees/GetAll",
+}).done((result) => {
+    console.log(result);
+    let countRequest = 0;
+    $.each(result, function (key, val) {
+        if (result[key].status == 0) {
+            countRequest += 1;
+        }
+    });
+    $("#emailNotification").html(countRequest);
+    $("#notifTitle").html("Ada " + countRequest + " pengajuan cuti yang belum diproses");
+
+    var notifBody = "";
+    $.each(result, function (key, val) {
+        if (result[key].status == 0) {
+            notifBody += `<div class="notif-center">
+                        <a href="#">
+                            <div class="notif-icon notif-info"> <i class="fa fa-envelope"></i> </div>
+                            <div class="notif-content">
+                                <span class="block">
+                                    ${result[key].nik} - ${result[key].employee.firstName} ${result[key].employee.lastName} mengirim pengajuan cuti
+                                </span>
+                            </div>
+                        </a>
+                    </div>`;
+        }
+    });
+    $("#notifBody").html(notifBody);
+
+
+}).fail((error) => {
+    console.log(error)
+})
