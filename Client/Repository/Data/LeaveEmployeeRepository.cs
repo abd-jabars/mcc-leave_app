a47b1fe9c6e70db9ad237ae9813b2a7a45a540ed
Syed Abdul Jabar <7abdjabar@gmail.com>
2022-01-29 19:03:09 +0700

    add leave employees http client

---
+++ Client/Repository/Data/LeaveEmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿using API.Models;
+using Client.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client.Repository.Data
+{
+    public class LeaveEmployeeRepository : GeneralRepository<LeaveEmployee, int>
+    {
+        private readonly Address address;
+        private readonly string request;
+        private readonly HttpClient httpClient;
+        public LeaveEmployeeRepository(Address address, string request = "LeaveEmployees/") : base(address, request)
+        {
+            this.address = address;
+            this.request = request;
+            httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(address.link)
+            };
+        }
+    }
+}
