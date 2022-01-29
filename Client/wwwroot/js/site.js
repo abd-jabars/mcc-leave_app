76ceb5d627b9d74775c69b565e54a4a59d692dc1
Syed Abdul Jabar <7abdjabar@gmail.com>
2022-01-29 11:34:21 +0700

    put user role in view layout

--- Client/wwwroot/js/site.js
+++ Client/wwwroot/js/site.js
@@ -8,6 +8,11 @@
     var userEmail = `${result.email}`;
     $("#userEmail").html(userEmail);
 
+    if (result.roleName != null) {
+        var countRoles = result.roleName.length;
+        var userRole = result.roleName[countRoles - 1];
+        $("#userRole").html(userRole);
+    }
 }).fail((error) => {
     console.log(error)
 })
